2a449dd7f383b442b51489203ceb247c5aaf6580
Leo D Penrose <lpenrose@depaul.edu>
2022-06-08 00:15:16 -0500

    First Commit

---
+++ src/main/java/com/example/playinginthespring/PlayingInTheSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.playinginthespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayingInTheSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayingInTheSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/playinginthespring/PlayingInTheSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.playinginthespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayingInTheSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
