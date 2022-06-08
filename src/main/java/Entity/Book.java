ab7798e88cbef892581aa3a77fd767d921180b5d
Leo D Penrose <lpenrose@depaul.edu>
2022-06-08 01:00:48 -0500

    added folder structure, added props for book entity

---
+++ src/main/java/Entity/Book.java
@@ -0,0 +1,50 @@
+package Entity;
+
+public class Book {
+
+    String title,author,publisher;
+    int yearOfPublishing;
+
+    public Book() {
+
+    }
+
+    public Book(String title, String author, String publisher, int yearOfPublishing) {
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.yearOfPublishing = yearOfPublishing;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public int getYearOfPublishing() {
+        return yearOfPublishing;
+    }
+
+    public void setYearOfPublishing(int yearOfPublishing) {
+        this.yearOfPublishing = yearOfPublishing;
+    }
+}
